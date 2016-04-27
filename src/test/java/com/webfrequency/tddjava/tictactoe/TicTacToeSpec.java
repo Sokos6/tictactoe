170210d27507d944e2fcc92e1b3b686ab50e7f5e
Sokos6 <will@comicbooked.com>
2016-04-27 11:09:06 -0400

    first test for board completed. Fails for play method

--- src/test/java/com/webfrequency/tddjava/tictactoe/TicTacToeSpec.java
+++ src/test/java/com/webfrequency/tddjava/tictactoe/TicTacToeSpec.java
@@ -1,7 +1,27 @@
 package com.webfrequency.tddjava.tictactoe;
 
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 /**
  * Created by sokos6 on 4/27/16.
  */
 public class TicTacToeSpec {
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+    private TicTacToe ticTacToe;
+
+    @Before
+    public final void before() {
+        ticTacToe = new TicTacToe();
+    }
+
+    @Test
+    public void whenXOutsideBoardThenRuntimeException() {
+        exception.expect(RuntimeException.class);
+        ticTacToe.play(5, 2);
+    }
 }
